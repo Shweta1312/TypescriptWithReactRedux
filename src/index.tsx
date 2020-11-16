df1536cb814d5c8324d962a4d7baf1957da67f45
Shweta1312 <shawant11@gmail.com>
2020-11-16 23:09:07 +0530

    Component state

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,40 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 interface AppProps {
-  color: string;
+  color?: string;
 }
 
-class App extends React.Component<AppProps> {
+interface AppState {
+  counter: number;
+}
+
+class App extends React.Component<AppProps, AppState> {
+  //Property way
+  state = { counter: 0 };
+
+  //Constructor way
+  constructor(props: AppProps) {
+    super(props);
+
+    this.state = { counter: 0 };
+  }
+
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+
   render() {
-    return <div>{this.props.color}</div>;
+    return (
+      <div>
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        {this.state.counter}
+      </div>
+    );
   }
 }
 
-ReactDom.render(<App color="red" />, document.querySelector('#root'));
+ReactDom.render(<App />, document.querySelector('#root'));
