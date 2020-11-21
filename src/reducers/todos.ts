7bbdedc7d3fff81b6f657772c44d37e9ef69d80d
Shweta1312 <shawant11@gmail.com>
2020-11-21 22:48:04 +0530

    Tracking loading with component state

--- src/reducers/todos.ts
+++ src/reducers/todos.ts
@@ -1,6 +1,7 @@
 import { Todo, ActionTypes, Action } from '../actions';
 
 export const todosReducer = (state: Todo[] = [], action: Action) => {
+  //here switch acts as a type guard
   switch (action.type) {
     case ActionTypes.fetchTodos:
       return action.payload;
