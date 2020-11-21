b0cfae797420f924e4f96739246b72715417ee0b
Shweta1312 <shawant11@gmail.com>
2020-11-21 18:52:41 +0530

    Redux setup code

--- src/index.tsx
+++ src/index.tsx
@@ -1,46 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { JsxElement } from 'typescript';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { App } from './components/App';
+import { reducers } from './reducers';
 
-interface AppProps {
-  color?: string;
-}
+const store = createStore(reducers, applyMiddleware(thunk));
 
-const App = (props: AppProps): JSX.Element =>{
-return <div>{props.color}</div>
-}
-
-// interface AppState {
-//   counter: number;
-// }
-
-// class App extends React.Component<AppProps, AppState> {
-//   //Property way
-//   state = { counter: 0 };
-
-//   //Constructor way
-//   constructor(props: AppProps) {
-//     super(props);
-
-//     this.state = { counter: 0 };
-//   }
-
-//   onIncrement = (): void => {
-//     this.setState({ counter: this.state.counter + 1 });
-//   };
-//   onDecrement = (): void => {
-//     this.setState({ counter: this.state.counter - 1 });
-//   };
-
-//   render() {
-//     return (
-//       <div>
-//         <button onClick={this.onIncrement}>Increment</button>
-//         <button onClick={this.onDecrement}>Decrement</button>
-//         {this.state.counter}
-//       </div>
-//     );
-//   }
-// }
-
-ReactDom.render(<App color="red"/>, document.querySelector('#root'));
+ReactDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
