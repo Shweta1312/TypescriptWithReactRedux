d754ec1c3fde13a8a625e79d6a2ecc0697195017
Shweta1312 <shawant11@gmail.com>
2020-11-21 21:03:48 +0530

    Connecting component to redux store

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,22 @@
+import { connect } from 'react-redux';
 import React from 'react';
+import { Todo, fetchTodos } from '../actions';
+import { StoreState } from '../reducers';
 
-export class App extends React.Component {
+interface AppProps {
+  todos: Todo[];
+  fetchTodos(): any;
+}
+
+class _App extends React.Component<AppProps> {
   render() {
     return <div>Hello there!</div>;
   }
 }
+
+const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
+  return { todos };
+};
+
+//connect() function connects a React component to a Redux store
+export const App = connect(mapStateToProps, { fetchTodos })(_App);
--- src/reducers/todos.ts
+++ src/reducers/todos.ts
@@ -1,7 +1,7 @@
 import { Todo, FetchTodosAction } from '../actions/index';
 import { ActionTypes } from '../actions/types';
 
-export const todosReducer = (state: Todo[], action: FetchTodosAction) => {
+export const todosReducer = (state: Todo[] = [], action: FetchTodosAction) => {
   switch (action.type) {
     case ActionTypes.fetchTodos:
       return action.payload;
